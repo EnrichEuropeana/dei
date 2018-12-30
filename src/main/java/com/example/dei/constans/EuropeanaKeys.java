433c175f02c23a171d050c16c21868404e76a30f
Laura Rakiewicz <rakiewicz.laura@gmail.com>
2018-12-30 13:53:53 +0100

    EN-14 EN-18

---
+++ src/main/java/com/example/dei/constans/EuropeanaKeys.java
@@ -0,0 +1,8 @@
+package com.example.dei.constans;
+
+public class EuropeanaKeys {
+
+    public static final String apiUrl = "https://www.europeana.eu/api/v2/search.json";
+    public static final String key = "ZBRUkSJMz";
+
+}
