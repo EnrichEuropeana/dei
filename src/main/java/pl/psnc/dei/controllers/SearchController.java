556f245e1df3806eb5ed39869c0a12a92d43d32d
Marcin Heliński <helin@man.poznan.pl>
2019-01-11 12:44:49 +0100

    EN-21: first use of europeana-client

--- src/main/java/pl/psnc/dei/controllers/SearchController.java
+++ src/main/java/pl/psnc/dei/controllers/SearchController.java
@@ -3,6 +3,7 @@ package pl.psnc.dei.controllers;
 import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import pl.psnc.dei.service.SearchService;
 
@@ -17,7 +18,7 @@ public class SearchController {
         this.applicationContext = applicationContext;
     }
 
-    @GetMapping("/search")
+    @GetMapping(value = "/search", produces = "application/json")
     public String search(@RequestParam(value = "query") String query, @RequestParam(value = "qf", required = false) String qf) {
         if (query.isEmpty()) {
             query = QUERY_ALL;
--- src/main/java/pl/psnc/dei/service/SearchService.java
+++ src/main/java/pl/psnc/dei/service/SearchService.java
@@ -1,16 +1,55 @@
 package pl.psnc.dei.service;
 
+import eu.europeana.api.client.EuropeanaApi2Client;
+import eu.europeana.api.client.model.EuropeanaApi2Results;
+import eu.europeana.api.client.search.query.Api2Query;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 @Service
 public class SearchService {
 
+    private static final Logger log = LoggerFactory.getLogger(SearchService.class);
+
     @Value("${api.key}")
     private String apiKey;
 
+    @Value("${search.api.url}")
+    private String searchApiUrl;
+
+    @Value("${search.api.profile}")
+    private String searchApiProfile;
+
     public String search(String query, String queryFilter) {
-        // consider using europeana client instead of http request
-        return String.format("Using api key: %s. Query: %s. Query filter: %s. Implement searching to see real results!", apiKey, query, queryFilter);
+        EuropeanaApi2Client searchClient = new EuropeanaApi2Client(searchApiUrl, apiKey);
+        Api2Query api2query = buildQuery(query, queryFilter);
+        try {
+            long start = System.currentTimeMillis();
+            EuropeanaApi2Results response = (EuropeanaApi2Results) searchClient.search(api2query);
+            log.info("Query time: " + String.valueOf(System.currentTimeMillis() - start) + " ms.");
+            if (response != null) {
+                return response.toJSON();
+            }
+        } catch (IOException e) {
+            log.error("Could not execute search API request.", e);
+        }
+        return "Error occurred during search...";
+    }
+
+    private Api2Query buildQuery(String query, String queryFilter) {
+        Api2Query api2Query = new Api2Query();
+        api2Query.setGeneralTerms(query);
+        if (queryFilter != null && !queryFilter.isEmpty()) {
+            String[] filters = queryFilter.split("&");
+            for (String filter : filters) {
+                api2Query.addQueryRefinement(filter);
+            }
+        }
+        api2Query.setProfile(searchApiProfile);
+        return api2Query;
     }
 }
