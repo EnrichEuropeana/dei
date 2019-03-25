4668587b2c12d4a5039bcbff88619c7093dfb53a
pWoz <pwozniak@man.poznan.pl>
2019-03-25 22:03:47 +0100

    UrlBuilder test

---
+++ src/test/java/pl/psnc/dei/service/UrlBuilderTest.java
@@ -0,0 +1,21 @@
+package pl.psnc.dei.service;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.test.util.ReflectionTestUtils;
+import pl.psnc.dei.model.Project;
+
+public class UrlBuilderTest {
+
+    @Test
+    public void shouldReturnProperValues() {
+        UrlBuilder urlBuilder = new UrlBuilder();
+        ReflectionTestUtils.setField(urlBuilder, "transcriptionPlatformLocation", "sampleUrl");
+
+        Assert.assertEquals("sampleUrl/Project/all", urlBuilder.urlForAllProjects());
+        Project project = new Project();
+        project.setProjectId("sampleProjectID");
+        Assert.assertEquals("sampleUrl/Dataset/search?ProjectId=sampleProjectID", urlBuilder.urlForProjectDatasets(project));
+
+    }
+}
