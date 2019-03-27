5ba9b4440b37469e9ea0d74fda4842d11d311fc9
Marcin Heliński <helin@man.poznan.pl>
2019-03-27 09:58:21 +0100

    EN-48: service for records and projects assignment implemented

--- src/main/java/pl/psnc/dei/model/DAO/RecordsRepository.java
+++ src/main/java/pl/psnc/dei/model/DAO/RecordsRepository.java
@@ -2,8 +2,16 @@ package pl.psnc.dei.model.DAO;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import pl.psnc.dei.model.Dataset;
+import pl.psnc.dei.model.Project;
 import pl.psnc.dei.model.Record;
+
+import java.util.List;
 
 @Repository
 public interface RecordsRepository extends JpaRepository<Record, Long> {
+
+    List<Record> findAllByProject(Project project);
+
+    List<Record> findAllByDataset(Dataset dataset);
 }
