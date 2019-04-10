62b57388e69cf5587fba5b298473b3a4257aa1b5
pWoz <pwozniak@man.poznan.pl>
2019-04-10 12:13:55 +0200

    EN-51 Text field for import name and import name generator

---
+++ src/main/java/pl/psnc/dei/ui/components/ImportName.java
@@ -0,0 +1,31 @@
+package pl.psnc.dei.ui.components;
+
+import com.vaadin.flow.component.textfield.TextField;
+
+import java.util.Date;
+
+/**
+ * Default Vaadin text field extended by the algorithm for title creation.
+ *
+ * <p>
+ * Created by pwozniak on 4/10/19
+ */
+public class ImportName extends TextField {
+
+    private static String TITLE_PATTERN = "IMPORT_%s_%tFT%tT";
+
+    public ImportName() {
+        this.setPlaceholder("Select project first");
+        this.setLabel("Import name");
+        this.setWidth("350px");   //TODO if shouldn't be in here
+    }
+
+    @Override
+    public void setValue(String value) {
+        super.setValue(generateImportName(value));
+    }
+
+    private String generateImportName(String value) {
+        return String.format(TITLE_PATTERN, value, new Date(), new Date());
+    }
+}
--- src/main/java/pl/psnc/dei/ui/pages/ImportPage.java
+++ src/main/java/pl/psnc/dei/ui/pages/ImportPage.java
@@ -3,12 +3,14 @@ package pl.psnc.dei.ui.pages;
 import com.vaadin.flow.component.HasValue;
 import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 import pl.psnc.dei.model.DAO.RecordsRepository;
 import pl.psnc.dei.model.Project;
 import pl.psnc.dei.model.Record;
 import pl.psnc.dei.service.TranscriptionPlatformService;
 import pl.psnc.dei.ui.MainView;
+import pl.psnc.dei.ui.components.ImportName;
 import pl.psnc.dei.ui.components.ProjectSelectionComponent;
 
 import java.util.List;
@@ -21,8 +23,11 @@ import java.util.List;
 @Route(value = "import", layout = MainView.class)
 public class ImportPage extends HorizontalLayout {
 
+    private TextField importName = new ImportName();
+
     public ImportPage(RecordsRepository repo,
                       TranscriptionPlatformService transcriptionPlatformService) {
+        add(importName);
         add(
                 new ProjectSelectionComponent(transcriptionPlatformService, new ProjectChangeListener(), null));
 
@@ -37,6 +42,7 @@ public class ImportPage extends HorizontalLayout {
         @Override
         public void valueChanged(HasValue.ValueChangeEvent<Project> event) {
             Project project = (Project) event.getValue();
+            importName.setValue(project.getName());
         }
     }
 }
