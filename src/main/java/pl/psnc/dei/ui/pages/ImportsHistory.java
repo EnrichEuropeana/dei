3564113f04026b0b11bcaeb78f479010e6fc6807
pWoz <pwozniak@man.poznan.pl>
2019-04-15 13:03:51 +0200

    Imports history page

---
+++ src/main/java/pl/psnc/dei/ui/pages/ImportsHistory.java
@@ -0,0 +1,13 @@
+package pl.psnc.dei.ui.pages;
+
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+import pl.psnc.dei.ui.MainView;
+
+@Route(value = "history", layout = MainView.class)
+public class ImportsHistory extends VerticalLayout {
+
+    public ImportsHistory() {
+
+    }
+}
