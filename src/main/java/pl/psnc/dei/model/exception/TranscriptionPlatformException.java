eb07e9a69231f28bee847ab86ea9c3282e73f9fa
pWoz <pwozniak@man.poznan.pl>
2019-04-26 07:54:47 +0200

    EN-86 Empty methods

---
+++ src/main/java/pl/psnc/dei/model/exception/TranscriptionPlatformException.java
@@ -0,0 +1,11 @@
+package pl.psnc.dei.model.exception;
+
+/**
+ * Created by pwozniak on 4/26/19
+ */
+public class TranscriptionPlatformException extends Exception {
+
+    public TranscriptionPlatformException(String message){
+        super(message);
+    }
+}
