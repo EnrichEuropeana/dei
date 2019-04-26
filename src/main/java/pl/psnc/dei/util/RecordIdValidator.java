7c9f89b57c2cb1ea7a2d0660ae38b24849ca3886
Kamil Gronowski <kgronowski@man.poznan.pl>
2019-04-26 13:04:19 +0200

    EN-70 Updated pattern for recordId validation

--- src/main/java/pl/psnc/dei/util/RecordIdValidator.java
+++ src/main/java/pl/psnc/dei/util/RecordIdValidator.java
@@ -2,7 +2,7 @@ package pl.psnc.dei.util;
 
 public class RecordIdValidator {
 
-	private static final String RECORD_ID_PATTERN = "^/[0-9]+/[0-9a-zA-Z_]+$";
+	private static final String RECORD_ID_PATTERN = "^(?U)/\\p{Digit}+/[\\p{Graph}&&[^/]]+$";
 
 	public static boolean validate(String recordId) {
 		return recordId.matches(RECORD_ID_PATTERN);
--- src/test/java/pl/psnc/dei/util/RecordIdValidatorTest.java
+++ src/test/java/pl/psnc/dei/util/RecordIdValidatorTest.java
@@ -14,17 +14,23 @@ public class RecordIdValidatorTest {
 		String testRecordId2 = "/12345678/12345";
 		String testRecordId3 = "/12345/_my_record_";
 		String testRecordId4 = "/123/record";
+		String testRecordId5 = "/123/1234_żółć.żółw";
+		String testRecordId6 = "/123/1234_Jean-Marie_Le'Blanc";
+		String testRecordId7 = "/123/1234_Ὀδυσσεύς";
 
 		Assert.assertTrue(RecordIdValidator.validate(testRecordId1));
 		Assert.assertTrue(RecordIdValidator.validate(testRecordId2));
 		Assert.assertTrue(RecordIdValidator.validate(testRecordId3));
 		Assert.assertTrue(RecordIdValidator.validate(testRecordId4));
+		Assert.assertTrue(RecordIdValidator.validate(testRecordId5));
+		Assert.assertTrue(RecordIdValidator.validate(testRecordId6));
+		Assert.assertTrue(RecordIdValidator.validate(testRecordId7));
 	}
 
 	@Test
 	public void shouldNotValidateRecordId() {
 		String testRecordId1 = "/12345_12345_my_record";
-		String testRecordId2 = "/12345/my-record";
+		String testRecordId2 = "/12345/my record";
 		String testRecordId3 = "/abc/12345_my_record";
 		String testRecordId4 = "/12345_123/12345_my_record";
 		String testRecordId5 = "12345/12345_my_record";
