fcc854eca0f23e50948c02f0212e5e593154de2e
Grzegorz Szczepaniak <gszczepaniak@man.poznan.pl>
2019-04-30 11:43:19 +0200

    sending to TP with error handling

--- src/main/java/pl/psnc/dei/service/TranscriptionPlatformService.java
+++ src/main/java/pl/psnc/dei/service/TranscriptionPlatformService.java
@@ -1,20 +1,32 @@
 package pl.psnc.dei.service;
 
+import io.netty.channel.ChannelOption;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import io.netty.handler.timeout.WriteTimeoutHandler;
 import org.apache.jena.atlas.json.JsonObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 import pl.psnc.dei.exception.DEIHttpException;
+import pl.psnc.dei.exception.NotFoundException;
 import pl.psnc.dei.model.DAO.DatasetsReposotory;
 import pl.psnc.dei.model.DAO.ProjectsRepository;
+import pl.psnc.dei.model.DAO.RecordsRepository;
 import pl.psnc.dei.model.Dataset;
 import pl.psnc.dei.model.Project;
+import pl.psnc.dei.model.Record;
+import pl.psnc.dei.model.Transcription;
+import pl.psnc.dei.model.exception.TranscriptionPlatformException;
 import reactor.core.publisher.Mono;
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.tcp.TcpClient;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Service responsible for communication with Transcription Platform.
@@ -25,80 +37,181 @@ import java.util.List;
 @Service
 public class TranscriptionPlatformService {
 
-    private final WebClient webClient;
-    @Autowired
-    private ProjectsRepository projectsRepository;
-    @Autowired
-    private DatasetsReposotory datasetsReposotory;
-    private List<Project> availableProjects;
-    private UrlBuilder urlBuilder;
+	private static final int READ_TIMEOUT_IN_SECONDS = 5;
+	private static final int WRITE_TIMEOUT_IN_SECONDS = 5;
+	private static final int CONNECTION_TIMEOUT_IN_SECONDS = 1;
 
-    public TranscriptionPlatformService(UrlBuilder urlBuilder,
-                                        WebClient.Builder webClientBuilder) {
-        this.urlBuilder = urlBuilder;
-        this.webClient = webClientBuilder.baseUrl(urlBuilder.getBaseUrl()).build();
-    }
+	@Autowired
+	private ProjectsRepository projectsRepository;
 
-    public List<Project> getProjects() {
-        if (!availableProjectInitialized()) {
-            initAvailableProjects();
-        }
-        return availableProjects;
-    }
+	@Autowired
+	private DatasetsReposotory datasetsReposotory;
+	@Autowired
+	private RecordsRepository recordsRepository;
+	//TODO uncomment when merging with queue
+    /*@Autowired
+    private TaskQueueService taskQueueService;*/
 
-    public void getDatasetsFor(Project project) {
-        Dataset[] projectDatasets = this.webClient.get().uri(urlBuilder.urlForProjectDatasets(project)).retrieve().bodyToMono(Dataset[].class).block();
+	private List<Project> availableProjects;
+	private UrlBuilder urlBuilder;
+	private WebClient webClient;
 
-        for (Dataset projectDataset : projectDatasets) {
-            projectDataset.setProject(project);
-            project.getDatasets().add(projectDataset);
-        }
-    }
+	public TranscriptionPlatformService(UrlBuilder urlBuilder,
+										WebClient.Builder webClientBuilder) {
+		this.urlBuilder = urlBuilder;
+		configureWebClient(urlBuilder, webClientBuilder);
+	}
 
-    public void getDatasetsFor(List<Project> projects) {
-        for (Project project : projects) {
-            getDatasetsFor(project);
-        }
-    }
+	private void configureWebClient(UrlBuilder urlBuilder, WebClient.Builder webClientBuilder) {
+		TcpClient tcpClient = TcpClient.create()
+				.option(ChannelOption.CONNECT_TIMEOUT_MILLIS, CONNECTION_TIMEOUT_IN_SECONDS * 1000)
+				.doOnConnected(con -> con.addHandlerLast(new ReadTimeoutHandler(READ_TIMEOUT_IN_SECONDS))
+						.addHandlerLast(new WriteTimeoutHandler(WRITE_TIMEOUT_IN_SECONDS)));
 
-    public void refreshAvailableProjects() {
-        initAvailableProjects();
-        getDatasetsFor(availableProjects);
-        saveAvailableProjects();
-    }
+		this.webClient = webClientBuilder
+				.clientConnector(new ReactorClientHttpConnector(HttpClient.from(tcpClient)))
+				.baseUrl(urlBuilder.getBaseUrl())
+				.build();
+	}
 
-    public void sendRecord(JsonObject record) {
-        this.webClient.post()
-                .uri(urlBuilder.urlForRecords())
-                .body(BodyInserters.fromObject(record))
-                .retrieve()
-                .onStatus(HttpStatus::is4xxClientError, clientResponse -> Mono.error(new DEIHttpException(clientResponse.rawStatusCode(), clientResponse.statusCode().getReasonPhrase())))
-                .onStatus(HttpStatus::is5xxServerError, clientResponse -> Mono.error(new DEIHttpException(clientResponse.rawStatusCode(), clientResponse.statusCode().getReasonPhrase())))
-                .bodyToMono(String.class)
-                .block();
-    }
+	public List<Project> getProjects() {
+		if (!availableProjectInitialized()) {
+			initAvailableProjects();
+		}
+		return availableProjects;
+	}
 
-    private boolean availableProjectInitialized() {
-        return availableProjects != null;
-    }
+	public void getDatasetsFor(Project project) {
+		Dataset[] projectDatasets = this.webClient.get().uri(urlBuilder.urlForProjectDatasets(project)).retrieve().bodyToMono(Dataset[].class).block();
 
-    private void initAvailableProjects() {
-        Project[] projects = this.webClient.get().uri(urlBuilder.urlForAllProjects()).retrieve().bodyToMono(Project[].class).block();
-        availableProjects = Arrays.asList(projects);
-    }
+		for (Dataset projectDataset : projectDatasets) {
+			projectDataset.setProject(project);
+			project.getDatasets().add(projectDataset);
+		}
+	}
 
-    private void saveAvailableProjects() {
-        for (Project project : availableProjects) {
-            Project savedProject = projectsRepository.findByName(project.getName());
-            if (savedProject == null) {
-                savedProject = projectsRepository.save(project);
-            }
-            for (Dataset dataset : project.getDatasets()) {
-                if (datasetsReposotory.findDatasetByDatasetId(dataset.getDatasetId()) == null) {
-                    dataset.setProject(savedProject);
-                    datasetsReposotory.save(dataset);
-                }
-            }
-        }
-    }
+	public void getDatasetsFor(List<Project> projects) {
+		for (Project project : projects) {
+			getDatasetsFor(project);
+		}
+	}
+
+	public void refreshAvailableProjects() {
+		initAvailableProjects();
+		getDatasetsFor(availableProjects);
+		saveAvailableProjects();
+	}
+
+	/**
+	 * Sends record to TP
+	 *
+	 * @param record record that will be send to TP
+	 * @throws TranscriptionPlatformException
+	 */
+
+	public void sendRecord(JsonObject record) throws TranscriptionPlatformException {
+		this.webClient.post()
+				.uri(urlBuilder.urlForSendingRecord())
+				.body(BodyInserters.fromObject(record))
+				.retrieve()
+				.onStatus(HttpStatus::is4xxClientError, clientResponse -> Mono.error(new DEIHttpException(clientResponse.rawStatusCode(), clientResponse.statusCode().getReasonPhrase())))
+				.onStatus(HttpStatus::is5xxServerError, clientResponse -> Mono.error(new DEIHttpException(clientResponse.rawStatusCode(), clientResponse.statusCode().getReasonPhrase())))
+				.bodyToMono(String.class)
+				.doOnError(cause -> {
+					if (cause instanceof TranscriptionPlatformException) {
+						throw new TranscriptionPlatformException("Error while communicating with Transcription Platform");
+					} else {
+						throw new TranscriptionPlatformException("Error while communicating with Transcription Platform", cause);
+					}
+				})
+				.block();
+	}
+
+	/**
+	 * Reads all the transcriptions generated by the Transcription Platform for the given record
+	 *
+	 * @param record record that will be used for transcriptions fetching
+	 * @return list of all transcriptions for the given record
+	 * @throws TranscriptionPlatformException
+	 */
+	public List<Transcription> fetchTranscriptionsFor(Record record) throws TranscriptionPlatformException {
+		Transcription[] recordTranscriptions =
+				this.webClient
+						.get()
+						.uri(urlBuilder.urlForRecord(record))
+						.retrieve()
+						.onStatus(HttpStatus::is5xxServerError, clientResponse -> Mono.error(new TranscriptionPlatformException()))
+						.bodyToMono(Transcription[].class)
+						.doOnError(cause -> {
+							if (cause instanceof TranscriptionPlatformException) {
+								throw new TranscriptionPlatformException("Error while communicating with Transcription Platform");
+							} else {
+								throw new TranscriptionPlatformException("Error while communicating with Transcription Platform", cause);
+							}
+						})
+						.block();
+		return Arrays.asList(recordTranscriptions);
+	}
+
+	/**
+	 * Sends annotation url (generated by Annotations API) to the Transcription Platform
+	 *
+	 * @param transcription
+	 * @throws TranscriptionPlatformException
+	 */
+	public void sendAnnotationUrl(Transcription transcription) throws TranscriptionPlatformException {
+		this.webClient
+				.post()
+				.uri(urlBuilder.urlForTranscription(transcription))
+				.body(BodyInserters.fromObject(transcription.getAnnotationId()))
+				.retrieve()
+				.onStatus(HttpStatus::is5xxServerError, clientResponse -> Mono.error(new TranscriptionPlatformException()))
+				.bodyToMono(Object.class)
+				.doOnError(cause -> {
+					if (cause instanceof TranscriptionPlatformException) {
+						throw new TranscriptionPlatformException("Error while communicating with Transcription Platform");
+					} else {
+						throw new TranscriptionPlatformException("Error while communicating with Transcription Platform", cause);
+					}
+				})
+				.block();
+	}
+
+	public void createNewTranscribeTask(String recordId) throws NotFoundException {
+		Optional<Record> record = recordsRepository.findByIdentifier(recordId);
+		if (record.isPresent()) {
+			//TODO uncomment when merging with queue
+            /*Record savedRecord = record.get();
+            savedRecord.setState(Record.RecordState.T_PENDING);
+            recordsRepository.save(savedRecord);
+            TranscriptionTask transcriptionTask = new TranscriptionTask(record.get());
+            taskQueueService.addTaskToQueue(transcriptionTask);*/
+		} else {
+			throw new NotFoundException("Record not found.");
+		}
+	}
+
+	private boolean availableProjectInitialized() {
+		return availableProjects != null;
+	}
+
+	private void initAvailableProjects() {
+		Project[] projects = this.webClient.get().uri(urlBuilder.urlForAllProjects()).retrieve().bodyToMono(Project[].class).block();
+		availableProjects = Arrays.asList(projects);
+	}
+
+	private void saveAvailableProjects() {
+		for (Project project : availableProjects) {
+			Project savedProject = projectsRepository.findByName(project.getName());
+			if (savedProject == null) {
+				savedProject = projectsRepository.save(project);
+			}
+			for (Dataset dataset : project.getDatasets()) {
+				if (datasetsReposotory.findDatasetByDatasetId(dataset.getDatasetId()) == null) {
+					dataset.setProject(savedProject);
+					datasetsReposotory.save(dataset);
+				}
+			}
+		}
+	}
 }
--- src/main/java/pl/psnc/dei/service/UrlBuilder.java
+++ src/main/java/pl/psnc/dei/service/UrlBuilder.java
@@ -3,6 +3,8 @@ package pl.psnc.dei.service;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import pl.psnc.dei.model.Project;
+import pl.psnc.dei.model.Record;
+import pl.psnc.dei.model.Transcription;
 
 /**
  * Class responsible for building urls to different endpoints;
@@ -15,7 +17,8 @@ public class UrlBuilder {
     private static final String ALL_PROJECTS_SUFFIX = "/Project/all";
     private static final String DATASETS_SEARCH_SUFFIX = "/Dataset/search";
     private static final String IMPORTS_ADD_SUFFIX = "/";
-    private static final String RECORD_SUFFIX = "/record";
+    private static final String RECORDS_SUFFIX = "/records";
+    private static final String TRANSCRIPTIONS_SUFFIX = "/transcription";
 
     @Value("${transcription.api.url}")
     private String transcriptionPlatformLocation;
@@ -24,21 +27,29 @@ public class UrlBuilder {
         return transcriptionPlatformLocation;
     }
 
-    public String urlForRecords() {
-        return transcriptionPlatformLocation + RECORD_SUFFIX;
-    }
-
     public String urlForAllProjects() {
         return transcriptionPlatformLocation + ALL_PROJECTS_SUFFIX;
+    }
+
+    public String urlForTranscription(Transcription transcription) {
+        return transcriptionPlatformLocation + TRANSCRIPTIONS_SUFFIX + "/" + transcription.getTp_id();
     }
 
     public String urlForProjectDatasets(Project project) {
         return transcriptionPlatformLocation + DATASETS_SEARCH_SUFFIX + "?ProjectId=" + project.getProjectId();
     }
 
-    public String urlForSendingImport(){
+    public String urlForSendingImport() {
         return transcriptionPlatformLocation + IMPORTS_ADD_SUFFIX;
         //todo change to real url
+    }
+
+    public String urlForRecord(Record record) {
+        return transcriptionPlatformLocation + RECORDS_SUFFIX + "/" + record.getIdentifier();
+    }
+
+    public String urlForSendingRecord() {
+        return transcriptionPlatformLocation + RECORDS_SUFFIX;
     }
 
 }
