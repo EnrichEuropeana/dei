f716a37d31027c6b88e844fd0200ada32a77f535
Grzegorz Szczepaniak <gszczepaniak@man.poznan.pl>
2019-07-02 12:22:56 +0200

    logout-fix

--- src/main/java/pl/psnc/dei/config/SecurityConfig.java
+++ src/main/java/pl/psnc/dei/config/SecurityConfig.java
@@ -25,72 +25,72 @@ import org.springframework.security.web.authentication.session.SessionAuthentica
 @Configuration
 @EnableWebSecurity
 @ComponentScan(basePackageClasses = KeycloakSecurityComponents.class,
-        excludeFilters = @ComponentScan.Filter(type = FilterType.REGEX, pattern = "org.keycloak.adapters.springsecurity.management.HttpSessionManager"))
+		excludeFilters = @ComponentScan.Filter(type = FilterType.REGEX, pattern = "org.keycloak.adapters.springsecurity.management.HttpSessionManager"))
 class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
 
-    @Autowired
-    private KeycloakClientRequestFactory keycloakClientRequestFactory;
+	@Autowired
+	private KeycloakClientRequestFactory keycloakClientRequestFactory;
 
-    // Submits the KeycloakAuthenticationProvider to the AuthenticationManager
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        KeycloakAuthenticationProvider keycloakAuthenticationProvider = keycloakAuthenticationProvider();
-        SimpleAuthorityMapper simpleAuthorityMapper = new SimpleAuthorityMapper();
-        simpleAuthorityMapper.setPrefix("");
-        keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(simpleAuthorityMapper);
-        auth.authenticationProvider(keycloakAuthenticationProvider);
-    }
+	// Submits the KeycloakAuthenticationProvider to the AuthenticationManager
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		KeycloakAuthenticationProvider keycloakAuthenticationProvider = keycloakAuthenticationProvider();
+		SimpleAuthorityMapper simpleAuthorityMapper = new SimpleAuthorityMapper();
+		simpleAuthorityMapper.setPrefix("");
+		keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(simpleAuthorityMapper);
+		auth.authenticationProvider(keycloakAuthenticationProvider);
+	}
 
-    @Bean
-    public KeycloakSpringBootConfigResolver KeycloakConfigResolver() {
-        return new KeycloakSpringBootConfigResolver();
-    }
+	@Bean
+	public KeycloakSpringBootConfigResolver KeycloakConfigResolver() {
+		return new KeycloakSpringBootConfigResolver();
+	}
 
-    // Specifies the session authentication strategy
-    @Bean
-    @Override
-    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
-        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
-    }
+	// Specifies the session authentication strategy
+	@Bean
+	@Override
+	protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+		return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+	}
 
-    @Bean
-    public KeycloakRestTemplate createKeycloakRestTemplate( ) {
-        return new KeycloakRestTemplate(keycloakClientRequestFactory);
-    }
+	@Bean
+	public KeycloakRestTemplate createKeycloakRestTemplate( ) {
+		return new KeycloakRestTemplate(keycloakClientRequestFactory);
+	}
 
-    @Bean
-    public FilterRegistrationBean keycloakAuthenticationProcessingFilterRegistrationBean(
-            KeycloakAuthenticationProcessingFilter filter) {
-        FilterRegistrationBean registrationBean = new FilterRegistrationBean(filter);
-        registrationBean.setEnabled(false);
-        return registrationBean;
-    }
+	@Bean
+	public FilterRegistrationBean keycloakAuthenticationProcessingFilterRegistrationBean(
+			KeycloakAuthenticationProcessingFilter filter) {
+		FilterRegistrationBean registrationBean = new FilterRegistrationBean(filter);
+		registrationBean.setEnabled(false);
+		return registrationBean;
+	}
 
-    @Bean
-    public FilterRegistrationBean keycloakPreAuthActionsFilterRegistrationBean(
-            KeycloakPreAuthActionsFilter filter) {
-        FilterRegistrationBean registrationBean = new FilterRegistrationBean(filter);
-        registrationBean.setEnabled(false);
-        return registrationBean;
-    }
+	@Bean
+	public FilterRegistrationBean keycloakPreAuthActionsFilterRegistrationBean(
+			KeycloakPreAuthActionsFilter filter) {
+		FilterRegistrationBean registrationBean = new FilterRegistrationBean(filter);
+		registrationBean.setEnabled(false);
+		return registrationBean;
+	}
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
-        http.headers().frameOptions().sameOrigin();         //to enable h2 console
-        http.httpBasic().disable();
-        http.anonymous().disable();
-        http
-                .csrf().disable()
-                .authorizeRequests()
-                // allow Vaadin URLs and the login URL without authentication
-                .regexMatchers("/frontend/.*", "/VAADIN/.*").permitAll()
-                .requestMatchers(SecurityUtils::isFrameworkInternalRequest).permitAll()
-                .antMatchers("/accessdenied*", "/logout*", "/sso/login*").permitAll()
-                .and()
-                .authorizeRequests().anyRequest().hasAuthority("operator")
-                .and()
-                .requiresChannel()
-                    .requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null).requiresSecure();
-    }
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		super.configure(http);
+		http.headers().frameOptions().sameOrigin();         //to enable h2 console
+		http.httpBasic().disable();
+		http.anonymous().disable();
+		http
+				.csrf().disable()
+				.authorizeRequests()
+				// allow Vaadin URLs and the login URL without authentication
+				.regexMatchers("/frontend/.*", "/VAADIN/.*").permitAll()
+				.requestMatchers(SecurityUtils::isFrameworkInternalRequest).permitAll()
+				.antMatchers("/accessdenied*", "/logout*", "/sso/login*").permitAll()
+				.and()
+				.authorizeRequests().anyRequest().hasAuthority("operator")
+				.and()
+				.requiresChannel()
+				.requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null).requiresSecure();
+	}
 }
