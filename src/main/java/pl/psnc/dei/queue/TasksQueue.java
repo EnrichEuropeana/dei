ad1c14f01c95c0567032d25a5f06d2f8f7020bef
Grzegorz Szczepaniak <gszczepaniak@man.poznan.pl>
2019-07-09 12:31:21 +0200

    logging

--- src/main/java/pl/psnc/dei/queue/TasksQueue.java
+++ src/main/java/pl/psnc/dei/queue/TasksQueue.java
@@ -36,6 +36,7 @@ public class TasksQueue implements Runnable {
 			try {
 				Task task = tasks.take();
 				try {
+					logger.info("Processing record {} with state {}", task.getRecord().getIdentifier(), task.getRecord().getState());
 					task.process();
 					processingSuccessful();
 				} catch (Exception e) {
