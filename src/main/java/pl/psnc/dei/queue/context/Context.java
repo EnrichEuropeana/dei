00cb9a9ce7efab7c7e69a76ca989b285f2984460
Piotr Tylczynski <piotr@ptl.cloud>
2021-07-07 14:21:32 +0200

    conversion task context add

--- src/main/java/pl/psnc/dei/queue/context/Context.java
+++ src/main/java/pl/psnc/dei/queue/context/Context.java
@@ -1,15 +1,34 @@
 package pl.psnc.dei.queue.context;
 
+import pl.psnc.dei.model.Record;
 import pl.psnc.dei.queue.task.Task;
 
 public class Context {
-    public Task.TaskState getState() {
-        return state;
+    private Record.RecordState recordState;
+    private Task.TaskState taskState;
+    private Record record;
+
+    public Record.RecordState getRecordState() {
+        return recordState;
     }
 
-    public void setState(Task.TaskState state) {
-        this.state = state;
+    public void setRecordState(Record.RecordState recordState) {
+        this.recordState = recordState;
     }
 
-    private Task.TaskState state;
+    public Task.TaskState getTaskState() {
+        return taskState;
+    }
+
+    public void setTaskState(Task.TaskState taskState) {
+        this.taskState = taskState;
+    }
+
+    public Record getRecord() {
+        return record;
+    }
+
+    public void setRecord(Record record) {
+        this.record = record;
+    }
 }
---
+++ src/main/java/pl/psnc/dei/queue/context/ConversionTaskContext.java
@@ -0,0 +1,106 @@
+package pl.psnc.dei.queue.context;
+
+import org.hibernate.annotations.NaturalId;
+import pl.psnc.dei.model.Record;
+import pl.psnc.dei.queue.task.ConversionTask;
+
+import javax.persistence.*;
+
+@Entity
+public class ConversionTaskContext extends Context{
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NaturalId
+    @OneToOne
+    private Record record;
+
+    // PROCESSING STATE STORAGE
+    private boolean hasJson;
+    private boolean hasConverted;
+    private boolean hasThrownException;
+    private boolean hasAddedFailure;
+
+    // PROCESSING DATA STORAGE
+    private String recordJsonRaw;
+    private String recordJson;
+
+
+    public ConversionTaskContext from(ConversionTask task){
+        ConversionTaskContext context = new ConversionTaskContext();
+        context.setRecord(task.getRecord());
+        context.setHasJson(false);
+        context.setHasConverted(false);
+        context.setHasThrownException(false);
+        context.setHasAddedFailure(false);
+        context.setRecordJson("");
+        context.setRecordJsonRaw("");
+        return null;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public Record getRecord() {
+        return record;
+    }
+
+    @Override
+    public void setRecord(Record record) {
+        this.record = record;
+    }
+
+    public boolean isHasJson() {
+        return hasJson;
+    }
+
+    public void setHasJson(boolean hasJson) {
+        this.hasJson = hasJson;
+    }
+
+    public boolean isHasConverted() {
+        return hasConverted;
+    }
+
+    public void setHasConverted(boolean hasConverted) {
+        this.hasConverted = hasConverted;
+    }
+
+    public boolean isHasThrownException() {
+        return hasThrownException;
+    }
+
+    public void setHasThrownException(boolean hasThrownException) {
+        this.hasThrownException = hasThrownException;
+    }
+
+    public boolean isHasAddedFailure() {
+        return hasAddedFailure;
+    }
+
+    public void setHasAddedFailure(boolean hasAddedFailure) {
+        this.hasAddedFailure = hasAddedFailure;
+    }
+
+    public String getRecordJsonRaw() {
+        return recordJsonRaw;
+    }
+
+    public void setRecordJsonRaw(String recordJsonRaw) {
+        this.recordJsonRaw = recordJsonRaw;
+    }
+
+    public String getRecordJson() {
+        return recordJson;
+    }
+
+    public void setRecordJson(String recordJson) {
+        this.recordJson = recordJson;
+    }
+}
