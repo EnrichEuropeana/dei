240310327993ba4ecd670cb9ea5d15fe49bf3942
Kacper Wleklak <kacperwleklak@gmail.com>
2021-07-08 13:00:05 +0200

    fix lombok

--- src/main/java/pl/psnc/dei/model/ImportFailure.java
+++ src/main/java/pl/psnc/dei/model/ImportFailure.java
@@ -3,14 +3,16 @@ package pl.psnc.dei.model;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.Date;
 
 @Entity
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class ImportFailure {
