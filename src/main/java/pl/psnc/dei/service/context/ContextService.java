f8120c07ed1b26cb458702ea0ee076a05bd65e4e
Piotr Tylczynski <piotr@ptl.cloud>
2021-07-15 13:45:25 +0200

    docs on context service add

--- src/main/java/pl/psnc/dei/service/context/ContextService.java
+++ src/main/java/pl/psnc/dei/service/context/ContextService.java
@@ -2,6 +2,10 @@ package pl.psnc.dei.service.context;
 
 import pl.psnc.dei.model.Record;
 
+/**
+ * Service used for manipulation on concrete service
+ * @param <T> context class being served by this service
+ */
 public abstract class ContextService<T> {
     public abstract T get (Record record);
     public abstract T save (T context);
